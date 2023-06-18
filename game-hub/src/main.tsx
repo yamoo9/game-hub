5088d3546226d7fd8127e2d38d3857e87ca055ba
yamoo9 <yamoo9@naver.com>
2023-06-18 14:26:29 +0900

    컬러 모드 설정

--- game-hub/src/main.tsx
+++ game-hub/src/main.tsx
@@ -1,14 +1,17 @@
 import { StrictMode } from 'react';
-import { ChakraProvider } from '@chakra-ui/react';
+import { ChakraProvider, ColorModeScript } from '@chakra-ui/react';
 import { createRoot } from 'react-dom/client';
-import App from './App';
+
 import './index.css';
+import App from './App';
+import theme from './theme';
 
 const rootElement = document.getElementById('root') as HTMLElement;
 
 createRoot(rootElement).render(
   <StrictMode>
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
+      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
       <App />
     </ChakraProvider>
   </StrictMode>
