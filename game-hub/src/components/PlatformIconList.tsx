620284ab8104e60e35bdf8ccbf37cf9d23fb0e68
yamoo9 <yamoo9@naver.com>
2023-06-18 18:26:38 +0900

    플랫폼 아이콘 표시

---
+++ game-hub/src/components/PlatformIconList.tsx
@@ -0,0 +1,48 @@
+import {
+  FaWindows,
+  FaPlaystation,
+  FaXbox,
+  FaApple,
+  FaLinux,
+  FaAndroid,
+} from 'react-icons/fa';
+import { MdPhoneIphone } from 'react-icons/md';
+import { SiNintendo } from 'react-icons/si';
+import { BsGlobe } from 'react-icons/bs';
+import { IconType } from 'react-icons';
+
+import { HStack, Icon } from '@chakra-ui/react';
+import { Platform } from '../hooks/useGames';
+
+const iconMap: { [key: string]: IconType } = {
+  playstation: FaPlaystation,
+  xbox: FaXbox,
+  nintendo: SiNintendo,
+  pc: FaWindows,
+  mac: FaApple,
+  linux: FaLinux,
+  ios: MdPhoneIphone,
+  android: FaAndroid,
+  web: BsGlobe,
+};
+
+interface Props {
+  platforms: Platform[];
+}
+
+function PlatformIconList({ platforms }: Props) {
+  return (
+    <HStack mt={2}>
+      {platforms.map((platform) => (
+        <Icon
+          as={iconMap[platform.slug]}
+          aria-label={platform.name}
+          title={platform.name}
+          color="gray.500"
+        />
+      ))}
+    </HStack>
+  );
+}
+
+export default PlatformIconList;
