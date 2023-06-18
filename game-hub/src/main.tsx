a75a7444750f0cad7a43cbd4ffdde1704d0c4212
yamoo9 <yamoo9@naver.com>
2023-06-18 09:47:46 +0900

    Chakra UI 설치 및 구성

--- game-hub/src/main.tsx
+++ game-hub/src/main.tsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { StrictMode } from 'react';
+import { ChakraProvider } from '@chakra-ui/react';
+import { createRoot } from 'react-dom/client';
+import App from './App';
+import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const rootElement = document.getElementById('root') as HTMLElement;
+
+createRoot(rootElement).render(
+  <StrictMode>
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
+  </StrictMode>
+);
