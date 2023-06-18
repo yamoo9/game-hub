bb6f01d76046808c341ae8b0be53d850ad4c84ab
yamoo9 <yamoo9@naver.com>
2023-06-18 17:07:58 +0900

    useGames 훅 작성

--- game-hub/src/components/GameGrid.tsx
+++ game-hub/src/components/GameGrid.tsx
@@ -1,35 +1,14 @@
 import { Text } from '@chakra-ui/react';
-import { useEffect, useState } from 'react';
-import apiClient from '../services/api-client';
-
-interface Game {
-  id: number;
-  name: string;
-}
-
-interface FetchGamesResponse {
-  count: number;
-  results: Game[];
-}
+import useGames from '../hooks/useGames';
 
 function GameGrid() {
-  const [gameList, setGameList] = useState<Game[]>([]);
-  const [error, setError] = useState<string>('');
-
-  useEffect(() => {
-    apiClient
-      .get<FetchGamesResponse>('/games')
-      .then((response) => {
-        setGameList(response.data.results);
-      })
-      .catch((error: Error) => setError(error.message));
-  }, []);
+  const { games, error } = useGames();
 
   return (
     <>
       {error && <Text>{error}</Text>}
       <ul>
-        {gameList.map((game) => (
+        {games.map((game) => (
           <li key={game.id}>{game.name}</li>
         ))}
       </ul>
---
+++ game-hub/src/hooks/useGames.ts
@@ -0,0 +1,38 @@
+import { CanceledError } from 'axios';
+import { useEffect, useState } from 'react';
+import apiClient from '../services/apiClient';
+
+interface Game {
+  id: number;
+  name: string;
+}
+
+interface FetchGamesResponse {
+  count: number;
+  results: Game[];
+}
+
+function useGames() {
+  const [games, setGames] = useState<Game[]>([]);
+  const [error, setError] = useState<string>('');
+
+  useEffect(() => {
+    const controller = new AbortController();
+
+    apiClient
+      .get<FetchGamesResponse>('/games', { signal: controller.signal })
+      .then((response) => {
+        setGames(response.data.results);
+      })
+      .catch((error) => {
+        if (error instanceof CanceledError) return;
+        setError(error.message);
+      });
+
+    return () => controller.abort();
+  }, []);
+
+  return { games, error };
+}
+
+export default useGames;
