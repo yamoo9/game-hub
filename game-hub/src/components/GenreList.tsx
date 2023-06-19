b71caed2aa242584343bd0ecb07c31544efd53ee
yamoo9 <yamoo9@naver.com>
2023-06-19 11:32:05 +0900

    로딩 스피너 구성

--- game-hub/src/components/GenreList.tsx
+++ game-hub/src/components/GenreList.tsx
@@ -1,9 +1,49 @@
-import { HStack, Image, List, ListItem, Text } from '@chakra-ui/react';
+import {
+  Alert,
+  AlertDescription,
+  AlertIcon,
+  AlertTitle,
+  Box,
+  HStack,
+  Image,
+  List,
+  ListItem,
+  Spinner,
+  Text,
+} from '@chakra-ui/react';
 import useGenres from '../hooks/useGenres';
 import getCroppedImageUrl from '../services/imageUrl';
 
 function GenreList() {
   const { data: genres, error, isLoading } = useGenres();
+
+  if (isLoading) {
+    return (
+      <Box mt={3} mx={5}>
+        <Spinner
+          thickness="2px"
+          speed="1.2s"
+          emptyColor="gray.300"
+          color="cyan.500"
+          size="md"
+        />
+      </Box>
+    );
+  }
+
+  if (error) {
+    return (
+      <Alert status="error" borderRadius={6} ml={1}>
+        <AlertIcon />
+        <Box>
+          <AlertTitle>ERROR</AlertTitle>
+          <AlertDescription fontSize="sm" display="inline-block" lineHeight="1">
+            {error}
+          </AlertDescription>
+        </Box>
+      </Alert>
+    );
+  }
 
   return (
     <List ml={4} mt={3}>
