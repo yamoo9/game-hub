27929eee2e5991b2913aa907a27a45946d6d9a0d
yamoo9 <yamoo9@naver.com>
2023-06-20 14:44:53 +0900

    장르, 플랫폼 데이터 정적 처리

--- game-hub/src/hooks/usePlatforms.ts
+++ game-hub/src/hooks/usePlatforms.ts
@@ -1,4 +1,4 @@
-import useData from './useData';
+import platforms from '../data/platforms';
 
 export interface Platform {
   id: number;
@@ -6,6 +6,6 @@ export interface Platform {
   slug: string;
 }
 
-const usePlatforms = () => useData<Platform>('/platforms/lists/parents');
+const usePlatforms = () => ({ data: platforms, isLoading: false, error: '' });
 
 export default usePlatforms;
