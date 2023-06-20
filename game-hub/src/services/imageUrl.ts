ab6383e342bec73559e1452e6c0adc05c61c2b40
yamoo9 <yamoo9@naver.com>
2023-06-20 09:46:30 +0900

    Placeholder 이미지 구성

--- game-hub/src/services/imageUrl.ts
+++ game-hub/src/services/imageUrl.ts
@@ -1,5 +1,7 @@
+import placeholderImage from '../assets/placeholder-image.svg';
+
 function getCroppedImageUrl(url: string): string {
-  return url?.replace('media/', 'media/crop/600/400/');
+  return url ? url.replace('media/', 'media/crop/600/400/') : placeholderImage;
 }
 
 export default getCroppedImageUrl;
